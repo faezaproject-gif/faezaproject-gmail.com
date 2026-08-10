882d53731e3bd1854cd249c1b69235089787121d
faezaproject-gif <faezaproject@gmail.com>
2026-08-10 14:19:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+// ===============================
+// MENU MOBILE
+// ===============================
 
+const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu");
+
+menuButton.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+});
+
+
+// ===============================
+// TUTUP MENU SETELAH LINK DIKLIK
+// ===============================
+
+const navLinks = navMenu.querySelectorAll("a");
+
+navLinks.forEach(function (link) {
+
+    link.addEventListener("click", function () {
+
+        navMenu.classList.remove("active");
+
+    });
+
+});
+
+
+// ===============================
+// SMOOTH SCROLL
+// ===============================
+
+document.querySelectorAll('a[href^="#"]').forEach(function (link) {
+
+    link.addEventListener("click", function (e) {
+
+        const target = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        if (target) {
+
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+
+        }
+
+    });
+
+});
